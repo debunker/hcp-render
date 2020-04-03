3f5acb0a176bd09b5e06fc040f460566047c30da
Hardik Joshi <hardik.joshi@expereo.com>
2020-04-03 20:10:13 +0200

    hcp-render

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/u/0/uc?id=1MKDNQ9R1dpmeHI2wKWyCIF261WwhySNu&export=download'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['benjamina', 'lily', 'elastica']
 path = Path(__file__).parent
 
 app = Starlette()
